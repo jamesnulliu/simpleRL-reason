aa8c51b8e294e28db7ff6d507890e078c6626998
jamesnulliu <jamesnulliu@gmail.com>
2025-12-19 22:19:24 +0000

    [update]

--- eval/math_eval.py
+++ eval/math_eval.py
@@ -310,74 +310,72 @@ def main(llm, tokenizer, data_name, args):
             # )  # sort outputs by request_id
             outputs = [output.outputs[0].text for output in outputs]
         else:
-            outputs = generate_completions(
-                model=llm,
-                tokenizer=tokenizer,
-                prompts=prompts,
-                max_new_tokens=args.max_tokens_per_call,
-                batch_size=16,
-                stop_id_sequences=stop_words,
-            )
+            raise NotImplementedError("Only vllm is supported now.")
 
         assert len(outputs) == len(current_prompts)
 
-        # process all outputs
-        remain_prompts = []
-        remain_codes = []
-        for (i, query), output in zip(current_prompts, outputs):
-            output = output.rstrip()
-            query += output
-            if args.prompt_type == "pal":
-                remain_prompts.append((i, query))
-                if "```python" in output:
-                    output = extract_program(query)
-                remain_codes.append(output)
-            elif args.prompt_type == "cot":
-                end_prompts.append((i, query))
-            # elif "boxed" not in output and output.endswith("```"): # disable code execution
-            #     program = extract_program(query)
-            #     remain_prompts.append((i, query))
-            #     remain_codes.append(program)
-            else:
-                end_prompts.append((i, query))
+        # # process all outputs
+        # remain_prompts = []
+        # remain_codes = []
+        # for (i, query), output in zip(current_prompts, outputs):
+        #     output = output.rstrip()
+        #     query += output
+        #     if args.prompt_type == "pal":
+        #         remain_prompts.append((i, query))
+        #         if "```python" in output:
+        #             output = extract_program(query)
+        #         remain_codes.append(output)
+        #     elif args.prompt_type == "cot":
+        #         end_prompts.append((i, query))
+        #     # elif "boxed" not in output and output.endswith("```"): # disable code execution
+        #     #     program = extract_program(query)
+        #     #     remain_prompts.append((i, query))
+        #     #     remain_codes.append(program)
+        #     else:
+        #         end_prompts.append((i, query))
 
-        # execute the remain prompts
-        # assert len(remain_codes)==0
-        remain_results = executor.batch_apply(remain_codes)
-        for k in range(len(remain_prompts)):
-            # assert False
-            i, query = remain_prompts[k]
-            res, report = remain_results[k]
-            exec_result = res if res else report
-            if "pal" in args.prompt_type:
-                exec_result = "\\boxed{" + exec_result + "}"
-            exec_result = f"\n```output\n{exec_result}\n```\n"
-            query += exec_result
-            # not end
-            if epoch == max_func_call - 1:
-                query += "\nReach max function call limit."
-            remain_prompts[k] = (i, query)
+        # # execute the remain prompts
+        # # assert len(remain_codes)==0
+        # remain_results = executor.batch_apply(remain_codes)
+        # for k in range(len(remain_prompts)):
+        #     # assert False
+        #     i, query = remain_prompts[k]
+        #     res, report = remain_results[k]
+        #     exec_result = res if res else report
+        #     if "pal" in args.prompt_type:
+        #         exec_result = "\\boxed{" + exec_result + "}"
+        #     exec_result = f"\n```output\n{exec_result}\n```\n"
+        #     query += exec_result
+        #     # not end
+        #     if epoch == max_func_call - 1:
+        #         query += "\nReach max function call limit."
+        #     remain_prompts[k] = (i, query)
 
-    # unsolved samples
-    print("Unsolved samples:", len(remain_prompts))
-    end_prompts.extend(remain_prompts)
-    # sort by idx
-    end_prompts = sorted(end_prompts, key=lambda x: x[0])
+    # # unsolved samples
+    # print("Unsolved samples:", len(remain_prompts))
+    # end_prompts.extend(remain_prompts)
+    # # sort by idx
+    # end_prompts = sorted(end_prompts, key=lambda x: x[0])
 
-    # remove input_prompt from end_prompt
+    # # remove input_prompt from end_prompt
+    # codes = []
+    # assert len(input_prompts) == len(end_prompts)
+    # for i in range(len(input_prompts)):
+    #     _, end_prompt = end_prompts[i]
+    #     code = end_prompt.split(input_prompts[i])[-1].strip()
+    #     for stop_word in stop_words:
+    #         if stop_word in code:
+    #             code = code.split(stop_word)[0].strip()
+    #     codes.append(code)
     codes = []
-    assert len(input_prompts) == len(end_prompts)
-    for i in range(len(input_prompts)):
-        _, end_prompt = end_prompts[i]
-        code = end_prompt.split(input_prompts[i])[-1].strip()
-        for stop_word in stop_words:
-            if stop_word in code:
-                code = code.split(stop_word)[0].strip()
-        codes.append(code)
+    for output in outputs:
+        codes.append(output)
 
     # extract preds
     results = [
-        run_execute(executor, code, args.prompt_type, data_name) for code in codes
+        # run_execute(executor, code, args.prompt_type, data_name) for code in codes
+        (extract_answer(code, data_name), None)
+        for code in codes
     ]
     time_use = time.time() - start_time
 
@@ -388,22 +386,22 @@ def main(llm, tokenizer, data_name, args):
         result = results[i * args.n_sampling : (i + 1) * args.n_sampling]
         preds = [item[0] for item in result]
         reports = [item[1] for item in result]
-        for j in range(len(preds)):
-            if sample["gt"] in ["A", "B", "C", "D", "E"] and preds[j] not in [
-                "A",
-                "B",
-                "C",
-                "D",
-                "E",
-            ]:
-                preds[j] = choice_answer_clean(code[j])
-            elif is_multi_choice(sample["gt"]) and not is_multi_choice(preds[j]):
-                # remove any non-choice char
-                preds[j] = "".join(
-                    [c for c in preds[j] if c in ["A", "B", "C", "D", "E"]]
-                )
+        # for j in range(len(preds)):
+        #     if sample["gt"] in ["A", "B", "C", "D", "E"] and preds[j] not in [
+        #         "A",
+        #         "B",
+        #         "C",
+        #         "D",
+        #         "E",
+        #     ]:
+        #         preds[j] = choice_answer_clean(code[j])
+        #     elif is_multi_choice(sample["gt"]) and not is_multi_choice(preds[j]):
+        #         # remove any non-choice char
+        #         preds[j] = "".join(
+        #             [c for c in preds[j] if c in ["A", "B", "C", "D", "E"]]
+        #         )
 
-        sample.pop("prompt")
+        # sample.pop("prompt")
         sample.update({"code": code, "pred": preds, "report": reports})
         all_samples.append(sample)
 
